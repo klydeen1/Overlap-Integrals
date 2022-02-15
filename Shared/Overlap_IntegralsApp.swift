4ba1d1b0451c74a9b51b4e0c5270bbebd848ed71
Katelyn Lydeen <klydeen@hawk.iit.edu>
2022-02-14 23:12:51 -0600

    Initial Commit

---
+++ Shared/Overlap_IntegralsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Overlap_IntegralsApp.swift
+//  Shared
+//
+//  Created by Katelyn Lydeen on 2/14/22.
+//
+
+import SwiftUI
+
+@main
+struct Overlap_IntegralsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
